df6cc79095bd668b6c2483efb64cfde4f98c2c33
DavidSpartan95 <112651588+DavidSpartan95@users.noreply.github.com>
2023-10-17 11:36:25 +0200

    added news article struct

---
+++ Newspaper App/NewsArticle.swift
@@ -0,0 +1,15 @@
+//
+//  NewsArticle.swift
+//  Newspaper App
+//
+//  Created by David Ulvan on 2023-10-17.
+//
+
+import Foundation
+
+struct NewsArticle {
+    var title: String
+    var content: String
+    var date: Date
+    var urlImage: String
+}
---
+++ Newspaper App/NewsView.swift
@@ -0,0 +1,15 @@
+//
+//  NewsView.swift
+//  Newspaper App
+//
+//  Created by David Ulvan on 2023-10-17.
+//
+
+import Foundation
+
+
+struct NewsView: View {
+   
+    
+    
+}
