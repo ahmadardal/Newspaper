684c0758e375d51f9923088e66bb50dd2ec25a09
ahmadardal <ahmadardal@live.se>
2023-10-17 09:50:55 +0200

    Added project

---
+++ Newspaper App/Newspaper_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Newspaper_AppApp.swift
+//  Newspaper App
+//
+//  Created by Ahmad Ardal on 2023-10-17.
+//
+
+import SwiftUI
+
+@main
+struct Newspaper_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
